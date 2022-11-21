b9707830a2d8265787ba37fbf7fb387856339894
Tobi Akinwumi <tobiakinwumi@gmail.com>
2022-11-20 20:56:20 -0500

    initial commit

---
+++ components/WalletContextProvider.tsx
@@ -0,0 +1,27 @@
+// require("@solana/wallet-adapter-react-ui/styles.css");
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+import { FC, ReactNode } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { useMemo } from "react";
+
+const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const url = useMemo(() => clusterApiUrl("devnet"), []);
+  const phantom = new PhantomWalletAdapter();
+
+  return (
+    <ConnectionProvider endpoint={url}>
+      <WalletProvider wallets={[phantom]}>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletContextProvider;
